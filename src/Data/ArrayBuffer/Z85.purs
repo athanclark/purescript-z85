e2745cb756df680f3b31207015c69ed0fb163a44
Athan Clark <athan.clark@gmail.com>
2018-12-01 16:11:07 -0700

    tests failing, not sure exactly why

--- src/Data/ArrayBuffer/Z85.purs
+++ src/Data/ArrayBuffer/Z85.purs
@@ -15,58 +15,58 @@ import Data.ArrayBuffer.Types (Uint32Array)
 import Data.ArrayBuffer.Typed (dataView, asUint32Array)
 import Data.String.CodeUnits (toCharArray)
 import Data.String.Yarn (fromChars)
-import Data.Foldable (foldl)
-import Control.Monad.Maybe.Trans (MaybeT (..), runMaybeT)
 import Effect (Effect)
-import Effect.Class (liftEffect)
 import Effect.Unsafe (unsafePerformEffect)
-import Partial.Unsafe (unsafePartial)
+import Effect.Exception (throw)
 
 
 
-encodeZ85 :: Uint32Array -> String
+encodeZ85 :: Uint32Array -> Effect String
 encodeZ85 xs' =
   let xs = dataView xs'
-      ns :: Array Int
-      ns =
-        let q = byteLength xs `div` 4
-        in  0 .. (q - 1)
-      go :: String -> Int -> String
-      go acc n =
-        let word :: UInt
-            word = unsafePartial $ case unsafePerformEffect (getUint32le xs (n * 4)) of
-              Just w -> w
-            word' :: Array Char
-            word' = Vec.toArray (encodeWord word)
-        in  acc <> fromChars word'
-  in  foldl go "" ns
+      len = byteLength xs
+  in  if len == 0 then pure ""
+      else
+        let ns :: Array Int
+            ns =
+              let q = len `div` 4
+              in  0 .. (q - 1)
+            go :: String -> Int -> Effect String
+            go acc n = do
+              (mWord :: Maybe UInt) <- getUint32le xs (n * 4)
+              case mWord of
+                Nothing -> throw $ "Can't extract word: " <> show (n * 4)
+                Just word ->
+                  let word' :: Array Char
+                      word' = Vec.toArray (encodeWord word)
+                  in  pure (acc <> fromChars word')
+        in  Array.foldM go "" ns
 
 
-decodeZ85 :: String -> Maybe Uint32Array
+decodeZ85 :: String -> Effect Uint32Array
 decodeZ85 s =
   if charsLen `mod` 5 == 0
-    then
-      let run :: MaybeT Effect Uint32Array
-          run = do
-            let bytesLen = (charsLen `div` 5) * 4
-            bytes <- liftEffect (whole <$> ArrayBuffer.create bytesLen)
+    then do
+      let bytesLen = (charsLen `div` 5) * 4
+      bytes <- whole <$> ArrayBuffer.create bytesLen
 
-            let go :: Tuple Int (Array Z85Char) -> Char -> MaybeT Effect (Tuple Int (Array Z85Char))
-                go (Tuple byteOffset charsSoFar) c
-                  | Array.length charsSoFar /= 5 = pure $ Tuple byteOffset $ charsSoFar `Array.snoc` c
-                  | otherwise =
-                      let asVec = unsafePartial $ case Vec.fromArray charsSoFar of
-                            Just cs -> cs
-                      in  case decodeWord asVec of
-                            Nothing -> MaybeT (pure Nothing)
-                            Just word -> do
-                              liftEffect (setUint32le bytes word byteOffset)
-                              pure (Tuple (byteOffset + 4) [])
+      let go :: Tuple Int (Array Z85Char) -> Char -> Effect (Tuple Int (Array Z85Char))
+          go (Tuple byteOffset charsSoFar) c
+            | Array.length charsSoFar /= 5 = pure $ Tuple byteOffset $ charsSoFar `Array.snoc` c
+            | otherwise = do
+                asVec <- case Vec.fromArray charsSoFar of
+                  Just cs -> pure cs
+                  Nothing -> throw $ "Couldn't convert characters scanned so far to sized vector: "
+                                <> show charsSoFar
+                case decodeWord asVec of
+                  Nothing -> throw $ "Couldn't decode chunk to word: " <> show asVec
+                  Just word -> do
+                    setUint32le bytes word byteOffset
+                    pure (Tuple (byteOffset + 4) [])
 
-            void (Array.foldM go (Tuple 0 []) cs)
-            pure (asUint32Array bytes)
-      in  unsafePerformEffect (runMaybeT run)
-    else Nothing
+      void (Array.foldM go (Tuple 0 []) cs)
+      pure (asUint32Array bytes)
+    else throw "Serialized string is not multiple of 5"
   where
     cs :: Array Char
     cs = toCharArray s
--- test/Main.purs
+++ test/Main.purs
@@ -1,9 +1,54 @@
 module Test.Main where
 
+import Data.ArrayBuffer.Z85 (encodeZ85, decodeZ85)
+import Data.ArrayBuffer.Z85.Internal (encodeWord, decodeWord)
+
 import Prelude
+import Data.Maybe (Maybe (..))
+import Data.UInt (UInt)
+import Data.UInt as UInt
+import Data.Int (rem, toNumber)
+import Data.ArrayBuffer.ArrayBuffer (fromArray)
+import Data.ArrayBuffer.Types (Uint32Array)
+import Data.ArrayBuffer.DataView (whole)
+import Data.ArrayBuffer.Typed (asUint32Array)
+import Data.Array (dropEnd, length) as Array
 import Effect (Effect)
+import Effect.Unsafe (unsafePerformEffect)
 import Effect.Console (log)
+import Test.QuickCheck (arbitrary, quickCheckGen)
+import Test.QuickCheck.Gen (Gen, arrayOf)
 
 main :: Effect Unit
 main = do
-  log "You should add some tests."
+  log "Generating test cases..."
+  log "  - word serialization isomorphism"
+  quickCheckGen wordTest
+  log "  - sentence serialization isomorphism"
+  quickCheckGen sentenceTest
+
+
+wordTest :: Gen Boolean
+wordTest = do
+  x <- arbitraryUInt32
+  pure (Just x == decodeWord (encodeWord x))
+
+sentenceTest :: Gen Boolean
+sentenceTest = do
+  xs <- arbitraryUint32Array
+  let round1 = unsafePerformEffect (encodeZ85 xs)
+      round2 = unsafePerformEffect (decodeZ85 round1 >>= encodeZ85)
+  pure (round1 == round2)
+
+
+arbitraryUInt32 :: Gen UInt
+arbitraryUInt32 = (\x -> UInt.fromNumber x `mod` UInt.fromInt 32) <$> arbitrary
+
+
+arbitraryUint32Array :: Gen Uint32Array
+arbitraryUint32Array = do
+  let pruneRight xs =
+        let len = Array.length xs
+        in  Array.dropEnd (len `rem` 4)
+  xs <- arrayOf ((\x -> toNumber (x `mod` 32)) <$> arbitrary)
+  pure $ asUint32Array $ whole $ fromArray xs
