c6914eb5b04938ee88037ac7440baf84484560f5
Athan Clark <athan.clark@gmail.com>
2018-12-01 12:31:46 -0700

    using UInts

--- src/Data/ArrayBuffer/Z85/Internal.purs
+++ src/Data/ArrayBuffer/Z85/Internal.purs
@@ -4,8 +4,10 @@ import Prelude
 import Data.Maybe (Maybe (..))
 import Data.Array ((..))
 import Data.Array (index) as Array
-import Data.Int (pow)
-import Data.Int.Bits ((.&.), zshr)
+import Data.UInt (UInt, pow, zshr, (.&.))
+import Data.UInt (toInt, fromInt) as UInt
+-- import Data.Int (pow)
+-- import Data.Int.Bits ((.&.), zshr)
 import Data.Char (toCharCode)
 import Data.String.CodeUnits (charAt)
 import Data.Tuple.Native (t5, T5, prj)
@@ -17,10 +19,10 @@ import Partial.Unsafe (unsafePartial)
 
 
 -- | Represents a single base85 digit between `0` and `84`
-type Base85 = Int
+type Base85 = UInt
 
 -- | Represents a single base256 digit between `0x00` and `0xFF` - a Byte
-type Base256 = Int
+type Base256 = UInt
 
 -- | Character included in the z85 character set: `0-9`, `a-z`, `A-Z`, and `:+=^!/*?&<>()[]{}@%$#`
 type Z85Char = Char
@@ -35,14 +37,14 @@ z85Chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ.-:+=^
 
 
 lookupZ85Char :: Base85 -> Z85Char
-lookupZ85Char idx = unsafePartial $ case charAt idx z85Chars of
+lookupZ85Char idx = unsafePartial $ case charAt (UInt.toInt idx) z85Chars of
   Just c -> c
 
 
 -- | Reverse-sorted by the lookup index obtained by getting the UTF-16 char code value, minus 32 -
 -- | the equivalent of `"n".charCodeAt(0) - 32` in JavaScript, where `n` is some x85 character.
 charCodeToBase256 :: Array Base256
-charCodeToBase256 =
+charCodeToBase256 = map UInt.fromInt
   [ 0x00, 0x44, 0x00, 0x54, 0x53, 0x52, 0x48, 0x00
   , 0x4B, 0x4C, 0x46, 0x41, 0x00, 0x3F, 0x3E, 0x45
   , 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07
@@ -63,41 +65,53 @@ lookupBase256 c = Array.index charCodeToBase256 (toCharCode c - 32)
 
 
 -- | Encodes the value by extracting it out of a little-endian packed word, and packing it into five x85 chars
-encodeWord :: Int -> Z85Chunk
+encodeWord :: UInt -> Z85Chunk
 encodeWord word =
   let value = ST.run do
-        valueRef <- STRef.new 0
+        valueRef <- STRef.new (UInt.fromInt 0)
         for_ (0 .. 3) \j ->
-          let byteChunk :: Base256
-              byteChunk = (word `zshr` (8 * (4 - j - 1))) .&. 0xFF
-          in  void (STRef.modify (\val -> (val * 256) + byteChunk) valueRef)
+          let base256 :: Base256
+              base256 = UInt.fromInt 0xFF
+              byteShift :: UInt
+              byteShift = UInt.fromInt (8 * (4 - j - 1))
+              byteChunk :: Base256
+              byteChunk = (word `zshr` byteShift) .&. base256
+          in  void (STRef.modify (\val -> (val * UInt.fromInt 256) + byteChunk) valueRef)
         STRef.read valueRef
+      getChar :: Int -> Z85Char
       getChar n =
-        let divisor = 85 `pow` n
+        let divisor :: UInt
+            divisor = UInt.fromInt 85 `pow` UInt.fromInt n
             idx :: Base85
-            idx = (value `div` divisor) `mod` 85
+            idx = (value `div` divisor) `mod` UInt.fromInt 85
         in  lookupZ85Char idx
   in  t5 (getChar 4) (getChar 3) (getChar 2) (getChar 1) (getChar 0)
 
 
-decodeWord :: Z85Chunk -> Maybe Int
+decodeWord :: Z85Chunk -> Maybe UInt
 decodeWord chunk = case mBase256Values of
   Nothing -> Nothing
   Just base256Values ->
-    let value = ST.run do
-          valueRef <- STRef.new 0
-          let addPartValue part = void (STRef.modify (\val -> (val * 85) + part) valueRef)
+    let value :: UInt
+        value = ST.run do
+          valueRef <- STRef.new (UInt.fromInt 0)
+          let addPartValue part = void (STRef.modify (\val -> (val * UInt.fromInt 85) + part) valueRef)
           addPartValue (prj d0 base256Values)
           addPartValue (prj d1 base256Values)
           addPartValue (prj d2 base256Values)
           addPartValue (prj d3 base256Values)
           addPartValue (prj d4 base256Values)
           STRef.read valueRef
-        divisor n = 256 `pow` n
+        divisor :: Int -> UInt
+        divisor n = UInt.fromInt 256 `pow` UInt.fromInt n
     in  Just $ ST.run do
-          wordRef <- STRef.new 0
+          wordRef <- STRef.new (UInt.fromInt 0)
           for_ (3 .. 0) \n ->
-            void (STRef.modify (\word -> (word * 256) + ((value `div` divisor n) `mod` 256)) wordRef)
+            let go word =
+                  let magnitude = word * UInt.fromInt 256
+                      dust = (value `div` divisor n) `mod` UInt.fromInt 256
+                  in  magnitude + dust
+            in  void (STRef.modify go wordRef)
           STRef.read wordRef
   where
     mBase256Values :: Maybe (T5 Base256 Base256 Base256 Base256 Base256)
